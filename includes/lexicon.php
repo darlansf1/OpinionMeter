494439c5afb82afaa1c01b56370650cb3572d81c
Darlan Santana Farias <darlansf1@gmail.com>
2017-09-09 15:30:28 -0300

    improving readability, bug fixing on lexicon algorithm

--- includes/lexicon.php
+++ includes/lexicon.php
@@ -6,11 +6,10 @@ include_once 'HTTPTranslator.php';
 function getLexiconAsArray($polarity, $language){
 	if($language != 'pt' && $language != 'en')
 		return array();
-	
+
 	$lexicon = array();
 	$fileName = "includes/lexicon/".$polarity."-".$language."-stem.txt";
-	//$fileName = "includes/lexicon/negative-en.txt";
-	//echo $fileName;
+
 	$handle = fopen($fileName, "r");
 	if ($handle) {
 		while (($line = fgets($handle)) !== false) {
@@ -22,21 +21,11 @@ function getLexiconAsArray($polarity, $language){
 	} else {
 		phpAlert("Error when accessing lexicon file.");
 	}
-	
+
 	return $lexicon;
 }
 
-function getPolaritiesFromLexicon($mysqli, $aspect_suggestions, $translator, $language, $neighborhood){
-	$aspects = $aspect_suggestions[0];
-	$polarities = array();
-	$sentences = array();
-	$sentences_nospecial = array();
-	
-	//retrieving document text
-	$docText = getCurrDocText($mysqli);
-	if($docText == "")
-		return $polarities;
-	
+function getLexiconAccordingly($language, $translator) {
 	$positive_lexicon = array();
 	$negative_lexicon = array();
 	$stemLang = 'en';
@@ -48,42 +37,31 @@ function getPolaritiesFromLexicon($mysqli, $aspect_suggestions, $translator, $la
 		$positive_lexicon = getLexiconAsArray('positive', 'en');
 		$negative_lexicon = getLexiconAsArray('negative', 'en');
 	}
-	
-	$pointseparated = explode(".", ($docText));
-	$exclamationseparated = array();
-	
-	foreach($pointseparated as $sentence){
-		foreach(explode("!", $sentence) as $word){
-				array_push($exclamationseparated, $word);
-		}
-	}
-	
-	//$commaseparated = array();
-	foreach($exclamationseparated as $sentence){
-		foreach(explode("?", $sentence) as $word){
-				//array_push($commaseparated, $word);
-				array_push($sentences, $word);
-		}
-	}
-	
-	$noSpecials = array();
-	$docText = array();
+
+	return array($stemLang, $positive_lexicon, $negative_lexicon);
+}
+
+function breakIntoWordArrays($sentences) {
+	$wordArraySentencesNoSpecials = array();
+	$wordArraySentencesOriginals = array();
 	$text = "";
 	$sentence_index = 0;
 	$word_positioning = array();
+	//echo "sentences: ".var_dump($sentences)."<br>";
 	foreach($sentences as $sentence){
-		array_push($docText, array());
-		array_push($noSpecials, array());
+		//echo $sentence;
+		array_push($wordArraySentencesOriginals, array());
+		array_push($wordArraySentencesNoSpecials, array());
 		$word_index = 0;
 		foreach(explode(" ", $sentence) as $word){
 			$word = processWord($word);
 			if(strlen($word) > 0){
-				//echo "<ORIGINAL>".$word."</ORIGINAL>";
 				$clean_word = removeSpecialChars(utf8_encode(strtoupper(removeSpecialChars($word))));
-				array_push($noSpecials[$sentence_index], $clean_word);		
-				array_push($docText[$sentence_index], $word);
+				//echo "<br>including $word into word arrays<br>";
+				array_push($wordArraySentencesNoSpecials[$sentence_index], $clean_word);
+				array_push($wordArraySentencesOriginals[$sentence_index], $word);
 				$text = $text." ".$word;
-				if(array_key_exists($clean_word, $word_positioning)){
+				if(!array_key_exists($clean_word, $word_positioning)){
 					$word_positioning[$clean_word] = array();
 				}
 				array_push($word_positioning[$clean_word], $sentence_index);
@@ -93,55 +71,67 @@ function getPolaritiesFromLexicon($mysqli, $aspect_suggestions, $translator, $la
 		}
 		$sentence_index++;
 	}
-	
-	$sizes = array();
-	$size = 0;
-	for($i = 0; $i < count($docText); $i++){
-		array_push($sizes, $size);
-		$size = $size+count($docText[$i]);
+	return array($text, $wordArraySentencesNoSpecials, $wordArraySentencesOriginals, $word_positioning);
+}
+
+function getPolaritiesFromLexicon($mysqli, $aspect_suggestions, $translator, $language, $neighborhood){
+	$neighborhood = 5;
+
+	//retrieving document text
+	$docText = getCurrDocText($mysqli);
+	if($docText == "")
+		return array();
+
+	list($stemLang, $positive_lexicon, $negative_lexicon) = getLexiconAccordingly($language, $translator);
+	$sentences = preg_split('/(?<=[.?!])\s+(?=[a-z])/i', $docText);
+	list($text, $wordArraySentencesNoSpecials, $wordArraySentencesOriginals, $word_positioning) = breakIntoWordArrays($sentences);
+
+	$sentence_offsets = array();
+	$cumulative_size = 0;
+	for($i = 0; $i < count($wordArraySentencesOriginals); $i++){
+		array_push($sentence_offsets, $cumulative_size);
+		$cumulative_size = $cumulative_size+count($wordArraySentencesOriginals[$i]);
 	}
-	
+
 	$translations = array();
 	if($translator == 1){
 		$httpTranslator = new HTTPTranslator();
-		//echo "////text to be translated:$text////";
 		$translations = $httpTranslator->translate($text, $language);
-		
-		//echo "////translated length:".count($translations)."////";
 	}
-	
+
+	$polarities = array();
 	$keys = array_keys($aspect_suggestions[1]);
+	$aspects = $aspect_suggestions[0];
 	foreach($keys as $i){
 		$aspect = $aspects[$i];
-		
-		
-		$index1 = array_shift($word_positioning[$aspect]);
-		$index2 = array_shift($word_positioning[$aspect]);
-		
+		//echo "resolving for aspect $aspect<br>";
+
+		$sentence_index = array_shift($word_positioning[$aspect]);
+		$word_index = array_shift($word_positioning[$aspect]);
 		$polarity = 0;
-		$palavra = $noSpecials[$index1][$index2];
-		
+		$palavra = $wordArraySentencesNoSpecials[$sentence_index][$word_positioning];
+
 		//look around of the aspect for positive and negative words
-		for($j = max(0, $index2-$neighborhood); $j < min(count($docText[$index1]), $index2+$neighborhood); $j++){//goes word by word on the sentence
+		for($j = max(0, $word_index-$neighborhood); $j < min(count($wordArraySentencesOriginals[$sentence_index]), $word_index+$neighborhood); $j++){//goes word by word on the sentence
 			//if($j == $index2) continue;	//skip the aspect itself
-			
-			$term = $noSpecials[$index1][$j];
-			if($language != 'en' && $translator == 1)// && count($translations) > ($sizes[$index1])+$j)
-				$term = $translations[($sizes[$index1])+$j];
-			
-			//echo "<$aspect>".$term."</$aspect>";
+
+			$term = $wordArraySentencesNoSpecials[$sentence_index][$j];
+			if($language != 'en' && $translator == 1)// && count($translations) > ($sentence_offsets[$sentence_index])+$j)
+				$term = $translations[($sentence_offsets[$sentence_index])+$j];
+
 			$term = strtolower($term);
 			$term = doStem($term, $stemLang);
-			
+			//echo "stemmed term $term<br>";
+
 			if(array_search($term, $negative_lexicon) != ""){//give negative points if term is in the negative_lexicon, penalizes distance to aspect
-				$polarity-=count($docText[$index1])-abs($j-$index2);
-				//echo "<$aspect: negative>".$term."</$aspect>";
+				$polarity -= count($wordArraySentencesOriginals[$sentence_index])-abs($j-$word_index);
+				//echo "NEGATIVE ---".$term."<br>";
 			}if(array_search($term, $positive_lexicon) != ""){//give positive if term is in the positive_lexicon, penalizes distance to aspect
-				$polarity+=count($docText[$index1])-abs($j-$index2);
-				//echo "<$aspect: positive>".$term."</$aspect>";
+				$polarity += count($wordArraySentencesOriginals[$sentence_index])-abs($j-$word_index);
+				//echo "POSITIVE --- ".$term."<br>";
 			}
 		}
-		
+
 		$polarity = intval($polarity);
 		if($polarity > 0)
 			array_push($polarities, "POSITIVE");
@@ -150,22 +140,29 @@ function getPolaritiesFromLexicon($mysqli, $aspect_suggestions, $translator, $la
 		else
 			array_push($polarities, "NEUTRAL");
 	}
-	
+
 	return $polarities;
 }
 
 
 /* Unity test
-$fileName = "lexicon/negative-en.txt";
+	$fileName = "lexicon/negative-en.txt";
 	$handle = fopen($fileName, "r");
+  $text = "The bartender abandoned me";
+  $aspects = array();
+	array_push($aspects, array(0 => "BARTENDER"));
+	array_push($aspects, array(0 => 4));
+	echo var_dump($aspects);
+	$result = getPolaritiesFromLexiconTest($aspects, 0,"en", $text);
+	echo var_dump($result);
+
 	if ($handle) {
 		while (($line = fgets($handle)) !== false) {
-			echo("$line<br>");
+			#echo("$line<br>");
 		}
 
 		fclose($handle);
 	} else {
-		phpAlert("Error when accessing lexicon file.");
-	}
-*/
+		phpAlert("Error when accessing lexicon file on unit test.");
+	}*/
 ?>
